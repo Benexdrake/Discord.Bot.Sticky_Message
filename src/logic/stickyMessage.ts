e1a400314f0e534090b7079314fa0c6804067ef0
Benex Drake <benex@hotmail.de>
2023-08-31 08:32:19 +0100

    worked fine now

--- src/logic/stickyMessage.ts
+++ src/logic/stickyMessage.ts
@@ -1,17 +1,18 @@
-import { Message } from "discord.js";
+import { Message, TextChannel } from "discord.js";
 import {client} from '..'
+import config from '../../config.json';
 
 export class StickyMessage
 {
     async AddMessage(channelId:string, messageId:string)
     {
-      const insert = `insert or replace into sticky values ('${channelId}','${messageId}')`;
+      const insert = `insert or replace into sticky values ('${channelId}','${messageId}','${config.stickyMessagesChannel}')`;
       client.db?.get( insert );
     }
 
     async CheckMessage(interaction: Message)
     {
-      client.db?.get( "select messageid from sticky where channelid = " + `'${interaction.channelId}'`, [],
+      client.db?.get( `select messageid from sticky where Tochannelid = ${interaction.channelId}` , [],
       async (err: any,result: any) => 
       {
         if(result) 
@@ -23,16 +24,30 @@ export class StickyMessage
     {
       try 
       {
-        const oldMessage = await interaction.channel.messages.fetch(id);
-        // sende neue message
-        const newMessage = await interaction.channel.send({content: (await oldMessage).content, components:(await oldMessage).components});
-        if((oldMessage).deletable)
-        {
-          // AddMessage
-          await this.AddMessage(newMessage.channelId, newMessage.id);
-          // lösche alte message
-          (oldMessage).delete();
-        }
+        let channel = await interaction.guild?.channels.fetch(config.stickyMessagesChannel) as TextChannel;
+        
+        let stickyMessage = await channel?.messages?.fetch(id);
+
+        await interaction.channel.messages.fetch({limit:100}).then(async x => {
+          for(const m of x)
+          {
+            if(m[1].author.id === config.botID)
+            {
+              if(m[1].deletable)
+              {
+                const message = await interaction.channel.messages.fetch(m[1].id);
+                await message.delete();
+              }
+            }
+          }
+        });
+        
+
+        let content = ' '
+        if(stickyMessage.content !== '')
+          content = stickyMessage.content;
+        await interaction.channel.send({content: content, components: stickyMessage.components, embeds:stickyMessage.embeds});
+        
       } 
       catch 
       (error) 
